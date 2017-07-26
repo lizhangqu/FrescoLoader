6bf6aca52cf23c1eab0afdaebe1602d5d57b4dbc
李樟取 <lizhangqu@weidian.com>
2017-07-26 23:13:17 +0800

    * update sample

--- app/src/main/java/io/github/lizhangqu/sample/MainActivity.java
+++ app/src/main/java/io/github/lizhangqu/sample/MainActivity.java
@@ -4,13 +4,15 @@ import android.graphics.Color;
 import android.graphics.PointF;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.facebook.drawee.drawable.ScalingUtils;
+import java.util.ArrayList;
+import java.util.List;
 
 import io.github.lizhangqu.fresco.FrescoLoader;
 
@@ -23,6 +25,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         image = (ImageView) findViewById(R.id.image);
+
+        final List<Drawable> bgs = new ArrayList<Drawable>();
+        bgs.add(ContextCompat.getDrawable(this, R.mipmap.bg_zero));
+        bgs.add(ContextCompat.getDrawable(this, R.mipmap.bg_one));
+        bgs.add(ContextCompat.getDrawable(this, R.mipmap.bg_two));
+
+        final List<Drawable> overlays = new ArrayList<Drawable>();
+        overlays.add(ContextCompat.getDrawable(this, R.mipmap.overlay_one));
+        overlays.add(ContextCompat.getDrawable(this, R.mipmap.overlay_two));
+        overlays.add(ContextCompat.getDrawable(this, R.mipmap.overlay_three));
+
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,22 +52,22 @@ public class MainActivity extends AppCompatActivity {
                         .cornersRadii(10, 10, 10, 10)
                         .cornersRadius(10)
                         .roundAsCircle()
-                        .progressBar(new CircleProgressBarDrawable())
-                        .progressBarScaleType(ScalingUtils.ScaleType.CENTER_CROP)
-                        .placeholder(R.mipmap.ic_launcher_round)
-                        .placeholderScaleType(ScalingUtils.ScaleType.CENTER_CROP)
-                        .failure(R.mipmap.ic_launcher_round)
-                        .failureScaleType(ScalingUtils.ScaleType.CENTER_CROP)
-                        .retry(R.mipmap.ic_launcher_round)
-                        .retryScaleType(ScalingUtils.ScaleType.CENTER_CROP)
+                        .backgroundDrawable(ContextCompat.getDrawable(getApplicationContext(), R.mipmap.bg_zero))
+                        .progressBar(ContextCompat.getDrawable(getApplicationContext(), R.mipmap.icon_progress_bar))
+                        .progressBarScaleType(ImageView.ScaleType.CENTER_CROP)
+                        .placeholder(ContextCompat.getDrawable(getApplicationContext(), R.mipmap.icon_placeholder))
+                        .placeholderScaleType(ImageView.ScaleType.CENTER_CROP)
+                        .failure(ContextCompat.getDrawable(getApplicationContext(), R.mipmap.icon_failure))
+                        .failureScaleType(ImageView.ScaleType.CENTER_CROP)
+                        .retry(ContextCompat.getDrawable(getApplicationContext(), R.mipmap.icon_retry))
+                        .retryScaleType(ImageView.ScaleType.CENTER_CROP)
                         .colorFilter(new PorterDuffColorFilter(Color.RED, PorterDuff.Mode.DARKEN))
-                        .pressedStateOverlay(R.mipmap.ic_launcher_round)
-//                        .overlay(R.mipmap.ic_launcher_round)
-                        .backgroundDrawable(new ColorDrawable(Color.parseColor("#ee000000")))
-                        .actualScaleType(ScalingUtils.ScaleType.CENTER_CROP)
+                        .overlays(overlays)
+                        .pressedStateOverlay(ContextCompat.getDrawable(getApplicationContext(), R.mipmap.bg_one))
+                        .actualScaleType(ImageView.ScaleType.CENTER_CROP)
                         .lowerLoad(R.mipmap.ic_launcher_round)
 //                        .load("")//fail
-                        .load("http://photocdn.sohu.com/20160208/mp58375678_1454886851667_2.gif")//gif
+//                        .load("http://photocdn.sohu.com/20160208/mp58375678_1454886851667_2.gif")//gif
                         .load("http://desk.fd.zol-img.com.cn/t_s960x600c5/g5/M00/0D/01/ChMkJlgq0z-IC78PAA1UbwykJUgAAXxIwMAwQcADVSH340.jpg")
                         .localThumbnailPreviewsEnabled(true)
                         .into(image);
