3f606b63edbcf50ac05a951f1d8bf56a9f272aca
李樟取 <lizhangqu@weidian.com>
2019-06-03 09:47:56 +0800

    * update

--- library/src/main/java/io/github/lizhangqu/fresco/TagCompat.java
+++ library/src/main/java/io/github/lizhangqu/fresco/TagCompat.java
@@ -2,6 +2,8 @@ package io.github.lizhangqu.fresco;
 
 import android.view.View;
 
+import java.util.HashMap;
+
 /**
  * Tag兼容，可添加多个tag
  *
@@ -10,7 +12,6 @@ import android.view.View;
  * @since 2017-07-30 13:08
  */
 public class TagCompat {
-
     public static final String KEY_DRAWEE_HOLDER = "DraweeHolder";
 
     public static void setTag(View view, Object tag) {
@@ -21,11 +22,99 @@ public class TagCompat {
         return getTag(view, KEY_DRAWEE_HOLDER);
     }
 
-    public static void setTag(View view, String key, Object tag) {
-        com.android.support.application.TagCompat.setTag(view, key, tag);
+    private static final boolean COVER_TAG = true; //是否覆盖
+
+    /**
+     * 设置tag
+     *
+     * @param view   View
+     * @param tagKey tag key
+     * @param tagObj tag value
+     * @return 是否设置成功
+     */
+    public static boolean setTag(View view, String tagKey, Object tagObj) {
+        return setTag(view, tagKey, tagObj, COVER_TAG);
     }
 
-    public static Object getTag(View view, String key) {
-        return com.android.support.application.TagCompat.getTag(view, key);
+    /**
+     * 设置tag
+     *
+     * @param view        View
+     * @param tagKey      tag key
+     * @param tagObj      tag value
+     * @param overrideTag 是否覆盖非TagMap类型的已设置tag
+     * @return 是否设置成功
+     */
+    public static boolean setTag(View view, String tagKey, Object tagObj, boolean overrideTag) {
+        if (view != null) {
+            Object tag = view.getTag();
+            if (tag instanceof TagMap) {
+                ((TagMap) tag).put(tagKey, tagObj);
+                return true;
+            }
+            if (tag == null || overrideTag) {
+                view.setTag(new TagMap(tagKey, tagObj));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 获取tag
+     *
+     * @param view   View
+     * @param tagKey tag key
+     * @return tag value
+     */
+    public static Object getTag(View view, String tagKey) {
+        return getTag(view, tagKey, null);
+    }
+
+    /**
+     * 获取tag
+     *
+     * @param view         View
+     * @param tagKey       tag key
+     * @param defaultValue 默认值
+     * @return tag value
+     */
+    public static Object getTag(View view, String tagKey, Object defaultValue) {
+        if (view != null) {
+            Object tag = view.getTag();
+            if (tag instanceof TagMap) {
+                Object value = ((TagMap) tag).get(tagKey);
+                if (value != null) {
+                    return value;
+                }
+            }
+        }
+        return defaultValue;
+    }
+
+    /**
+     * 是否包含tag
+     *
+     * @param view   View
+     * @param tagKey tag key
+     * @return 是否含tag
+     */
+    public static boolean containsTag(View view, String tagKey) {
+        if (view != null && tagKey != null) {
+            Object tag = view.getTag();
+            if (tag instanceof TagMap) {
+                return ((TagMap) tag).containsKey(tagKey);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 存tag用的map
+     */
+    private static class TagMap extends HashMap<String, Object> {
+        private TagMap(String key, Object value) {
+            put(key, value);
+        }
     }
 }
